00696c246b43538b234ff08fd28c95f424d37c36
GioAH <53281888+GioAH@users.noreply.github.com>
2019-07-25 22:09:57 -0600

    Practica 1 actualizada

---
+++ ConsoleApp1/ConsoleApp1/Ejercicio2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Ejercicio2
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
